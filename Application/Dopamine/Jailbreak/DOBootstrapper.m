c08471fa5e08805cbdb9022deaa9a6fa01276e33
Mikasa-san <robin_san@outlook.com>
2025-04-13 08:06:41 +0400

    Update

--- Application/Dopamine/Jailbreak/DOBootstrapper.m
+++ Application/Dopamine/Jailbreak/DOBootstrapper.m
@@ -17,7 +17,7 @@
 #import <sys/stat.h>
 #import "NSString+Version.h"
 
-#define LIBKRW_DOPAMINE_BUNDLED_VERSION @"2.0.3"
+#define LIBKRW_DOPAMINE_BUNDLED_VERSION @"1.1.4"
 #define LIBROOT_DOPAMINE_BUNDLED_VERSION @"1.0.1"
 #define BASEBIN_LINK_BUNDLED_VERSION @"1.0.0"
 
@@ -587,7 +587,7 @@ Components: main\n\
 # Zebra Sources List\n\
 deb https://getzbra.com/repo/ ./\n\
 deb https://repo.chariz.com/ ./\n\
-deb https://yourepo.com/ ./\n\
+deb https://siieo.github.io/roothide/ ./\n\
 deb https://havoc.app/ ./\n\
 deb https://roothide.github.io/ ./\n\
 deb https://roothide.github.io/procursus iphoneos-arm64e/%d main\n\
